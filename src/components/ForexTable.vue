4b0c3d18654135d7610a2a28d3ad1e07968c0cef
nadz <leonard.ucarino@gmail.com>
2020-05-20 00:23:16 +0800

    added filter features and amount * rate features

--- src/components/ForexTable.vue
+++ src/components/ForexTable.vue
@@ -1,40 +1,61 @@
 <template>
-  <div>
-    <p v-if="!loaded">Please wait while fetching data ......</p>
-    <ProgressSpinner v-if="!loaded" />
-    <div v-if="loaded">
-      <Dropdown
-        v-model="base"
-        :options="baseArray"
-        :filter="true"
-        placeholder="Select a Base"
-        :showClear="true"
-        @change="filterData"
-      ></Dropdown>
-      <Dropdown
-        v-model="target"
-        :options="targetArray"
-        :filter="true"
-        placeholder="Select a Target"
-        :showClear="true"
-        @change="filterData"
-      ></Dropdown>
-      <DataTable :value="filteredData" :paginator="true" :rows="8">
-        <Column field="base" header="Base">
-          <template #body="slot">
-            <img :src="`https://www.countryflags.io/${slot.data.base.substr(0, 2)}/flat/32.png`" />
-            <span style="position: absolute; margin-left: .5em; margin-top:7px">{{slot.data.base}}</span>
-          </template>
-        </Column>
-        <Column field="target" header="Target">
-          <template #body="slot">
-            <img :src="`https://www.countryflags.io/${slot.data.target.substr(0, 2)}/flat/32.png`" />
-            <span style="position: absolute; margin-left: .5em; margin-top:7px">{{slot.data.target}}</span>
-          </template>
-        </Column>
-        <Column field="timestamp" header="Date"></Column>
-        <Column field="rate" header="Exchange Rate"></Column>
-      </DataTable>
+  <div class="p-fluid p-formgrid p-grid">
+    <div class="p-field p-col-12 p-md-12" v-if="!loaded">
+      <p>Please wait while fetching data ......</p>
+      <ProgressSpinner />
+    </div>
+    <div class="p-fluid p-formgrid p-grid" v-if="loaded">
+      <div class="p-field p-col-12 p-md-2">
+        <label>Base</label>
+        <Dropdown
+          v-model="base"
+          :options="baseArray"
+          placeholder="Select a Base"
+          :showClear="true"
+          @change="filterData"
+        ></Dropdown>
+      </div>
+      <div class="p-field p-col-12 p-md-2">
+        <label>Target</label>
+        <Dropdown
+          v-model="target"
+          :options="targetArray"
+          :filter="true"
+          placeholder="Select a Target"
+          :showClear="true"
+          @change="filterData"
+        ></Dropdown>
+      </div>
+      <div class="p-field p-col-12 p-md-2">
+        <label>Amount</label>
+        <InputNumber v-model="amount" showButtons :min="0" />
+      </div>
+      <div class="p-field p-col-12 p-md-12">
+        <DataTable :value="filteredData" :paginator="true" :rows="7" selectionMode="single">
+          <Column field="base" header="Base" :sortable="true">
+            <template #body="slot">
+              <img :src="`https://www.countryflags.io/${slot.data.base.substr(0, 2)}/flat/32.png`" />
+              <span style="position: absolute; margin-left: .5em; margin-top:7px">{{slot.data.base}}</span>
+            </template>
+          </Column>
+          <Column field="target" header="Target" :sortable="true">
+            <template #body="slot">
+              <img
+                :src="`https://www.countryflags.io/${slot.data.target.substr(0, 2)}/flat/32.png`"
+              />
+              <span
+                style="position: absolute; margin-left: .5em; margin-top:7px"
+              >{{slot.data.target}}</span>
+            </template>
+          </Column>
+          <Column field="timestamp" header="Date and Time"></Column>
+          <Column field="rate" header="Exchange Rate" :sortable="true">
+            <template #body="slot">
+              <span>{{ slot.data.rate.rate | currencyDecimal(amount) }}</span>
+            </template>
+          </Column>
+        </DataTable>
+      </div>
     </div>
   </div>
 </template>
@@ -46,6 +67,7 @@ export default {
   name: "ForexTable",
   data() {
     return {
+      amount: 1,
       loaded: false,
       forexData: null,
       filteredData: null,
@@ -83,7 +105,7 @@ export default {
         obj.base = datum[0].substr(0, 3);
         obj.target = datum[0].substr(3, 3);
         obj.timestamp = new Date(datum[1].timestamp * 1000).toLocaleString();
-        obj.rate = datum[1].rate.toFixed(3);
+        obj.rate = datum[1];
         array.push(obj);
       });
       return array;
@@ -117,6 +139,18 @@ export default {
         this.filteredData = this.forexData;
       }
     }
+  },
+  filters: {
+    currencyDecimal(rate, amount) {
+      return Number(rate * amount).toFixed(3);
+    }
   }
 };
 </script>
+
+<style scoped>
+.p-field.p-col-12.p-md-2 {
+  position: relative;
+  left: 25%;
+}
+</style>
